5acca5d51f2b5eb1a492253d586372757f9d0ff0
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-09-14 13:40:39 +0300

    xml files

--- app/src/main/java/com/cosmah/android_xml_files/MainActivity.java
+++ app/src/main/java/com/cosmah/android_xml_files/MainActivity.java
@@ -1,10 +1,13 @@
 package com.cosmah.android_xml_files;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView txtHello;
@@ -13,14 +16,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtHello = findViewById(R.id.txtHello);
-        txtHello.setText(getString(R.string.Hello));
+        txtHello = findViewById(R.id.txtHello); // Use the class variable here
+        txtHello.setText("Hello World");
     }
 
+    //OVERRIDING MENU(GETTING MENU)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    //onclick event listener
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.settingsmenu) {
+            Toast.makeText(this, "Setting selected", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.alrmMenu) {
+            Toast.makeText(this, "Alarm selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }else
+            return super.onOptionsItemSelected(item);
     }
 
 }
